a6e09f128b0b86d07c184fa7159355a569cbbf3f
chiyuns <45362196+chiyuns@users.noreply.github.com>
2019-10-18 17:23:43 +0800

    Update TestThread.cpp

--- Thread/TestThread.cpp
+++ Thread/TestThread.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+#include "Thread.h"
+#include <unistd.h>
 
+using namespace std;
+
+class TestThread :public Thread
+{
+public:
+	TestThread()
+	{
+		cout << "TestThread()" << endl;
+	}
+	~TestThread()
+	{
+		cout << "~TestThread()" << endl;
+	}
+private:
+	void run()
+	{
+		int i = 5;
+		while (i--)
+		{
+			cout << i << endl;
+			sleep(1);
+		}
+	}
+};
+
+
+int main()
+{
+	TestThread   *tt = new TestThread();
+	tt->setAutoDelete(true);
+	tt->start();
+	tt->join();
+	return 0;
+}
